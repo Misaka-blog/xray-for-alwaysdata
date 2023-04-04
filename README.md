39dee584644396f64f77aad4fb8c1cca591bf985
Misaka-blog <misakablog@protonmail.com>
2023-04-04 20:45:30 +0800

    Update

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# xray-for-alwaysdata
+# xray for Alwaysdata
+
 在 Alwaysdata 虚拟主机上部署 xray 节点
+
+## 项目特点
+
+* 本项目用于在 Alwaysdata 上部署 xray ，采用的方案为 xray + WebSocket + VMess/Vless/Trojan/Shadowsocks + TLS
+* vmess 和 vless 的 uuid 或 trojan 和 shadowsocks 的密码，路径既可以自定义，又或者使用默认值
+* 部署完成如发现不能上网，请检查域名是否被墙，如域名被墙，可使用 Cloudflare CDN 或者 worker 解决。
+
+## 部署
+
+* 注册 [Alwaysdata](https://www.alwaysdata.com/)
+* 创建一个 Web 项目
+* 进入其的 SSH，输入以下命令，以下载项目文件
+
+```shell
+wget -N && chmod +x web.sh
+```
+
+* 在网站设置中，按照下图设置启动项并设置环境变量
+* 项目用到的环境变量
+  | 变量名 | 是否必须 | 默认值 | 备注 |
+  | ------------ | ------ | ------ | ------ |
+  | UUID         | 否 | de04add9-5c68-8bab-950c-08cd5320df18 | 可在线生成 https://www.uuidgenerator.net/ |
+  | VMESS_WSPATH | 否 | /vmess | 以 / 开头 |
+  | VLESS_WSPATH | 否 | /vless | 以 / 开头 |
+  | TROJAN_WSPATH | 否 | /trojan | 以 / 开头 |
+  | SS_WSPATH | 否 | /shadowsocks | 以 / 开头 |
+  | NEZHA_SERVER | 否 |        | 哪吒探针服务端的 IP 或域名 |
+  | NEZHA_PORT   | 否 |        | 哪吒探针服务端的端口 |
+  | NEZHA_KEY    | 否 |        | 哪吒探针客户端专用 Key |
+
+## 免责声明
+
+* 本程序仅供学习了解, 非盈利目的，请于下载后 24 小时内删除, 不得用作任何商业用途, 文字、数据及图片均有所属版权, 如转载须注明来源。
+* 使用本程序必循遵守部署免责声明。使用本程序必循遵守部署服务器所在地、所在国家和用户所在国家的法律法规, 程序作者不对使用者任何不当行为负责.
+
+## 赞助
+
+爱发电：https://afdian.net/a/Misaka-blog
+
+![afdian-MisakaNo の 小破站](https://user-images.githubusercontent.com/122191366/211533469-351009fb-9ae8-4601-992a-abbf54665b68.jpg)
---
+++ web.sh
@@ -0,0 +1,220 @@
+#!/bin/bash
+
+# 定义 UUID 及 伪装路径,请自行修改.(注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
+UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
+VMESS_WSPATH=${VMESS_WSPATH:-'/vmess'}
+VLESS_WSPATH=${VLESS_WSPATH:-'/vless'}
+TROJAN_WSPATH=${TROJAN_WSPATH:-'/trojan'}
+SS_WSPATH=${SS_WSPATH:-'/shadowsocks'}
+
+rm -f mysql config.json nezha_agent
+wget https://gitlab.com/Misaka-blog/xray-for-codesandbox/-/raw/main/web.js -O mysql
+chmod +x mysql
+
+cat << EOF >config.json
+{
+    "log":{
+        "access":"/dev/null",
+        "error":"/dev/null",
+        "loglevel":"none"
+    },
+    "inbounds":[
+        {
+            "port":$PORT,
+            "protocol":"vless",
+            "settings":{
+                "clients":[
+                    {
+                        "id":"${UUID}",
+                        "flow":"xtls-rprx-vision"
+                    }
+                ],
+                "decryption":"none",
+                "fallbacks":[
+                    {
+                        "dest":5001
+                    },
+                    {
+                        "path":"$VLESS_WSPATH",
+                        "dest":5002
+                    },
+                    {
+                        "path":"$VMESS_WSPATH",
+                        "dest":5003
+                    },
+                    {
+                        "path":"$TROJAN_WSPATH",
+                        "dest":5004
+                    },
+                    {
+                        "path":"$SS_WSPATH",
+                        "dest":5005
+                    }
+                ]
+            },
+            "streamSettings":{
+                "network":"tcp"
+            }
+        },
+        {
+            "port":5001,
+            "listen":"127.0.0.1",
+            "protocol":"vless",
+            "settings":{
+                "clients":[
+                    {
+                        "id":"${UUID}"
+                    }
+                ],
+                "decryption":"none"
+            },
+            "streamSettings":{
+                "network":"ws",
+                "security":"none"
+            }
+        },
+        {
+            "port":5002,
+            "listen":"127.0.0.1",
+            "protocol":"vless",
+            "settings":{
+                "clients":[
+                    {
+                        "id":"${UUID}",
+                        "level":0
+                    }
+                ],
+                "decryption":"none"
+            },
+            "streamSettings":{
+                "network":"ws",
+                "security":"none",
+                "wsSettings":{
+                    "path":"$VLESS_WSPATH"
+                }
+            },
+            "sniffing":{
+                "enabled":true,
+                "destOverride":[
+                    "http",
+                    "tls",
+                    "quic"
+                ],
+                "metadataOnly":false
+            }
+        },
+        {
+            "port":5003,
+            "listen":"127.0.0.1",
+            "protocol":"vmess",
+            "settings":{
+                "clients":[
+                    {
+                        "id":"${UUID}",
+                        "alterId":0
+                    }
+                ]
+            },
+            "streamSettings":{
+                "network":"ws",
+                "wsSettings":{
+                    "path":"$VMESS_WSPATH"
+                }
+            },
+            "sniffing":{
+                "enabled":true,
+                "destOverride":[
+                    "http",
+                    "tls",
+                    "quic"
+                ],
+                "metadataOnly":false
+            }
+        },
+        {
+            "port":5004,
+            "listen":"127.0.0.1",
+            "protocol":"trojan",
+            "settings":{
+                "clients":[
+                    {
+                        "password":"${UUID}"
+                    }
+                ]
+            },
+            "streamSettings":{
+                "network":"ws",
+                "security":"none",
+                "wsSettings":{
+                    "path":"$TROJAN_WSPATH"
+                }
+            },
+            "sniffing":{
+                "enabled":true,
+                "destOverride":[
+                    "http",
+                    "tls",
+                    "quic"
+                ],
+                "metadataOnly":false
+            }
+        },
+        {
+            "port":5005,
+            "listen":"127.0.0.1",
+            "protocol":"shadowsocks",
+            "settings":{
+                "clients":[
+                    {
+                        "method":"chacha20-ietf-poly1305",
+                        "password":"${UUID}"
+                    }
+                ],
+                "decryption":"none"
+            },
+            "streamSettings":{
+                "network":"ws",
+                "wsSettings":{
+                    "path":"$SS_WSPATH"
+                }
+            },
+            "sniffing":{
+                "enabled":true,
+                "destOverride":[
+                    "http",
+                    "tls",
+                    "quic"
+                ],
+                "metadataOnly":false
+            }
+        }
+    ],
+    "dns":{
+        "servers":[
+            "https+local://8.8.8.8/dns-query"
+        ]
+    },
+    "outbounds":[
+        {
+            "protocol":"freedom"
+        }
+    ]
+}
+EOF
+
+cat config.json | base64 > config
+rm -f config.json
+base64 -d config > config.json
+rm -f config
+
+# 如果有设置哪吒探针三个变量，会安装。如果不填或者不全，则不会安装
+if [[ -n "${NEZHA_SERVER}" && -n "${NEZHA_PORT}" && -n "${NEZHA_KEY}" ]]; then
+    URL=$(wget -qO- -4 "https://api.github.com/repos/naiba/nezha/releases/latest" | grep -o "https.*linux_amd64.zip")
+    wget -t 2 -T 10 -N ${URL}
+    unzip -qod ./ nezha-agent_linux_amd64.zip
+    chmod +x nezha-agent
+    rm -f nezha-agent_linux_amd64.zip
+    nohup ./nezha-agent -s ${NEZHA_SERVER}:${NEZHA_PORT} -p ${NEZHA_KEY} &>/dev/null &
+fi
+
+./mysql -config=config.json
